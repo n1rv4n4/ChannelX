9411c507f0a343b7a9393b8087881c4980ce473d
Alper Karayaman <32897369+karayaman@users.noreply.github.com>
2017-11-01 10:13:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask,session, redirect, url_for, render_template, request
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send,emit, join_room, leave_room
 from flask_wtf import Form
 from wtforms.fields import StringField, SubmitField,PasswordField
 from wtforms.validators import Required
@@ -32,6 +32,7 @@ def join()
 
 @app.route('/',methods=['GET','POST'])
 def get_login():
+    print("Get Login")
     form = LoginForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
@@ -42,41 +43,55 @@ def get_login():
         form.password.data = session.get('password', '')
     return render_template('login.html', form=form)
 
-@app.route('/get_user_panel')
+@app.route('/get_user_panel',methods=['GET','POST'])
 def get_user_panel():
-	form=ChannelForm()
-	if form.validate_on_submit():
+    print("Get User Panel")
+    form=ChannelForm()
+    if form.validate_on_submit():
 		session['channel_name']=form.channel_name.data
 		session['nickname']=form.nickname.data
-		return redirect(url_for('/channel/'+form.channel_name))
-	return render_template('channel.html',channel=form.channel_name.data)
+		return redirect(url_for('get_channel'))
+    return render_template('user_panel.html',form=form)
 
-@socketio.on('joined', namespace='/channel')
+@app.route('/channel',methods=['GET','POST'])
+def get_channel():
+    print("Get Channel")
+    nickname = session.get('nickname', '')
+    channel_name = session.get('channel_name', '')
+    return render_template('channel.html', nickname=nickname, channel_name=channel_name)
+
+
+@socketio.on('joined',namespace='/channel')
 def joined(message):
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
-    channel = session.get('channel_name')
+    print("Message joined: ")
+    print(message)
+    channel_name = session.get('channel_name')
     nickname=session.get('nickname')
-    join_room(channel)
-    emit('status', {'msg': nickname + ' has entered the room.'}, channel=channel)
+    join_room(channel_name)
+    emit('status', {'msg': nickname + ' has entered the room.'}, room=channel_name)
 
 
-@socketio.on('text', namespace='/channel')
+@socketio.on('text',namespace='/channel')
 def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
-    channel = session.get('channel_name')
+    print("Message is ")
+    print(message)
+    channel_name = session.get('channel_name')
     nickname=session.get('nickname')
-    emit('message', {'msg': nickname + ':' + message['msg']}, channel=channel)
+    emit('message', {'msg': nickname + ':' + message['msg']}, room=channel_name)
 
 
-@socketio.on('left', namespace='/channel')
+@socketio.on('left',namespace='/channel')
 def left(message):
     """Sent by clients when they leave a room.
     A status message is broadcast to all people in the room."""
-    channel = session.get('channel_name')
-    leave_room(room)
-    emit('status', {'msg': session.get('nickname') + ' has left the room.'}, channel=channel)
+    print("Leaving room")
+    channel_name = session.get('channel_name')
+    leave_room(channel_name)
+    emit('status', {'msg': session.get('nickname') + ' has left the room.'}, room=channel_name)
 
 if __name__ == '__main__':
 	socketio.run(app)
