2a3ef35706475259567b4d107505c6879cfd9307
n1rv4n4 <o540az@gmail.com>
2017-11-15 10:07:09 +0300

    Compability & Bootstrap

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 from flask import Flask,session, redirect, url_for, render_template, request
 from flask_socketio import SocketIO, send,emit, join_room, leave_room
-from flask_wtf import Form
+from flask_wtf import FlaskForm		# Using FlaskForm rather than form eliminates deprecation warning.
 from wtforms.fields import StringField, SubmitField,PasswordField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager,UserMixin,login_user,login_required,logout_user
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))	# Relative path for SQLAlchemy database file.
 
-
-class LoginForm(Form):
-    """Accepts a name and a password."""
-    name = StringField('Name', validators=[Required()])
+class LoginForm(FlaskForm):
+    """ Accepts a name and a password. """
+    name = StringField('User Name', validators=[Required()])
     password = PasswordField('Password', validators=[Required()])
-    submit = SubmitField('Sign in')
+    submit = SubmitField('Sign In')
 
-class ChannelForm(Form):
-    """Accepts a channel_name and a nickname."""
+class ChannelForm(FlaskForm):
+    """ Accepts a channel_name and a nickname. """
     channel_name = StringField('Channel Name', validators=[Required()])
-    nickname = StringField('Nick Name', validators=[Required()])
-    submit = SubmitField('Join')
-
+    nickname = StringField('Nickname', validators=[Required()])
+    submit = SubmitField('Join!')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ChannelX'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////home/ubuntu/Desktop/ChannelX/login.db'
+app.config['SECRET_KEY'] = 'ChannelX!^+%&/(()=?798465312-_*'	# Random complex key for CSRF security.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False				# Eliminates SQLAlchemy deprecation warning.
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'login.db')
 db=SQLAlchemy(app)
 login_manager=LoginManager()
 login_manager.init_app(app)
@@ -39,7 +40,8 @@ class ChannelHistory(db.Model):
     username=db.Column(db.String(20))
     nickname=db.Column(db.String(20))
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='gevent')	# Working with gevent mode provides keyboard interrupt with CTRL+C.
+db.create_all()		# Creates DB.
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -47,81 +49,82 @@ def load_user(user_id):
 
 @app.route('/',methods=['GET','POST'])
 def get_login():
-    print("Get Login")
-    form = LoginForm()
-    if form.validate_on_submit():
-        user=User.query.filter_by(username=form.name.data).first()
-        print("username"+str(user))
-        if not user:
-            new_user=User(username=form.name.data,password=form.password.data)
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(new_user)
-        else:
-            if user.password != form.password.data:
-                print("Wrong Password")
-                return redirect('/')
-            login_user(user)
-        session['name'] = form.name.data
-        session['password'] = form.password.data
-        return redirect('/get_user_panel')
-    elif request.method == 'GET':
-        form.name.data = session.get('name', '')
-        form.password.data = session.get('password', '')
-    return render_template('login.html', form=form)
+	FlaskForm = LoginForm()
+	if FlaskForm.validate_on_submit():
+		print("Someone in login panel.")
+		user=User.query.filter_by(username=FlaskForm.name.data).first()
+		if not user:
+			new_user=User(username=FlaskForm.name.data,password=FlaskForm.password.data)
+			db.session.add(new_user)
+			db.session.commit()
+			login_user(new_user) 
+			print(FlaskForm.name.data + " has logged in.")
+		else:
+			if user.password != FlaskForm.password.data:
+				print("Someone tried to log in with wrong password!")
+				return redirect('/')
+			login_user(user)
+		session['name'] = FlaskForm.name.data
+		session['password'] = FlaskForm.password.data
+		return redirect('/get_user_panel')
+	elif request.method == 'GET':
+		FlaskForm.name.data = session.get('name')
+		FlaskForm.password.data = session.get('password')
+	return render_template('login.html', FlaskForm=FlaskForm)
 
 @app.route('/log_out')
 @login_required
 def log_out():
-    logout_user()
-    return 'Logged out'
+	logout_user()
+	print(session.get('name') + " has logged out.") 
+	return 'Logged out.'
 
 
 @app.route('/get_user_panel',methods=['GET','POST'])
 @login_required
 def get_user_panel():
-    print("Get User Panel")
+    print(session.get('name') + " in user panel.")
     username=session['name']
     channels=ChannelHistory.query.filter_by(username=username)
-    form=ChannelForm()
-    if form.validate_on_submit():
+    FlaskForm=ChannelForm()
+    if FlaskForm.validate_on_submit():
         if channels:
             flag=False
             for channel in channels:
-                if channel.channel_name == form.channel_name.data:
+                if channel.channel_name == FlaskForm.channel_name.data:
                     flag=True
                     session['channel_name']=channel.channel_name
                     session['nickname']=channel.nickname
             if not flag:
-                new_channel=ChannelHistory(channel_name=form.channel_name.data,nickname=form.nickname.data,username=username)
+                new_channel=ChannelHistory(channel_name=FlaskForm.channel_name.data,nickname=FlaskForm.nickname.data,username=username)
                 db.session.add(new_channel)
                 db.session.commit()
-                session['channel_name']=form.channel_name.data
-                session['nickname']=form.nickname.data
+                session['channel_name']=FlaskForm.channel_name.data
+                session['nickname']=FlaskForm.nickname.data
+                print(session.get('name') + " selected a nickname " + session.get('nickname'))
         else:
-            new_channel=ChannelHistory(channel_name=form.channel_name.data,nickname=form.nickname.data,username=username)
+            new_channel=ChannelHistory(channel_name=FlaskForm.channel_name.data,nickname=FlaskForm.nickname.data,username=username)
             db.session.add(new_channel)
             db.session.commit()
-            session['channel_name']=form.channel_name.data
-            session['nickname']=form.nickname.data
+            session['channel_name']=FlaskForm.channel_name.data
+            session['nickname']=FlaskForm.nickname.data
         return redirect(url_for('get_channel'))
-    return render_template('user_panel.html',form=form,channels=[channel.channel_name for channel in channels])
+    return render_template('user_panel.html',FlaskForm=FlaskForm,channels=[channel.channel_name for channel in channels])
 
 @app.route('/channel',methods=['GET','POST'])
 @login_required
 def get_channel():
-    print("Get Channel")
-    nickname = session.get('nickname', '')
-    channel_name = session.get('channel_name', '')
+    print(session.get('nickname') + " created channel " + session.get('channel_name'))
+    nickname = session.get('nickname')
+    channel_name = session.get('channel_name')
     return render_template('channel.html', nickname=nickname, channel_name=channel_name)
 
 
 @socketio.on('joined',namespace='/channel')
 def joined(message):
-    """Sent by clients when they enter a channel.
-    A status message is broadcast to all people in the channel."""
-    print("Message joined: ")
-    print(message)
+    """ Sent by clients when they enter a channel.
+    A status message is broadcast to all people in the channel. """
+    print(session.get('nickname') + " entered the channel " + session.get('channel_name'))
     channel_name = session.get('channel_name')
     nickname=session.get('nickname')
     join_room(channel_name)
@@ -130,20 +133,19 @@ def joined(message):
 
 @socketio.on('text',namespace='/channel')
 def text(message):
-    """Sent by a client when the user entered a new message.
-    The message is sent to all people in the channel."""
-    print("Message is ")
-    print(message)
+    """ Sent by a client when the user entered a new message.
+    The message is sent to all people in the channel. """
+    print("Sent message by " + session.get('nickname') + ": " + message['msg'])
     channel_name = session.get('channel_name')
     nickname=session.get('nickname')
-    emit('message', {'msg': nickname + ':' + message['msg']}, room=channel_name)
+    emit('message', {'msg': nickname + ': ' + message['msg']}, room=channel_name)
 
 
 @socketio.on('left',namespace='/channel')
 def left(message):
-    """Sent by clients when they leave a channel.
-    A status message is broadcast to all people in the channel."""
-    print("Leaving channel")
+    """ Sent by clients when they leave a channel.
+    A status message is broadcast to all people in the channel. """
+    print(session.get('nickname') + " leaved the channel " + session.get('channel_name'))
     channel_name = session.get('channel_name')
     leave_room(channel_name)
     emit('status', {'msg': session.get('nickname') + ' has left the channel.'}, room=channel_name)
