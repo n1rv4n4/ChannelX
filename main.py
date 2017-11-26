64e8f2f705d22e31a1d0513bcc8aefff29c683b3
n1rv4n4 <o540az@gmail.com>
2017-11-26 18:36:49 +0300

    Channel password, design improvements.

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from flask import Flask, session, redirect, url_for, render_template, request
 from flask_socketio import SocketIO, send,emit, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-import os, datetime
+import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))	# Relative path for SQLAlchemy database file.
 app = Flask(__name__)
 
+app.config['g-recaptcha-response'] = '6Ld9yzgUAAAAAO75KNjOiT4QH5uCbn6sl_WzdHHU'
 app.config['SECRET_KEY'] = 'ChannelX!^+%&/(()=?798465312-_*'	# Random complex key for CSRF security.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False				# Eliminates SQLAlchemy deprecation warning.
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///' + os.path.join(basedir, 'database.db')
@@ -24,10 +25,11 @@ class User(UserMixin, db.Model):
 	Email=db.Column(db.String(20))
 
 class Channel(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    channel_name=db.Column(db.String(20))
-    Username=db.Column(db.String(20))
-    nickname=db.Column(db.String(20))
+	id=db.Column(db.Integer,primary_key=True)
+	Channel_Name=db.Column(db.String(20))
+	Channel_Password=db.Column(db.String(20))
+	Username=db.Column(db.String(20))
+	Nickname=db.Column(db.String(20))
 
 socketio = SocketIO(app, async_mode='gevent')	# Working with gevent mode provides keyboard interrupt with CTRL+C.
 db.create_all()		# Creates DB.
@@ -37,9 +39,12 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 @app.route('/', methods=['GET'])
-def home_page():
-    now = datetime.datetime.now()
+def home():
     return render_template('index.html')
+
+@app.route('/terms_and_conditions', methods=['GET'])
+def terms_and_conditions():
+    return render_template('terms_and_conditions.html')
 
 @app.route('/sign_up', methods = ['GET','POST'])
 def sign_up():
@@ -51,19 +56,19 @@ def sign_up():
 		Confirm = request.form['Confirm']
 		usr = User.query.filter_by(Username = Username).first()
 		if usr:
-		    username_error = "Username already exists choose another one."
-		    return render_template('sign_up.html', username_error = username_error)
+		    username_failure = "Username already exists choose another one."
+		    return render_template('sign_up.html', username_failure = username_failure)
 		else:
 			if Password == Confirm:
 				new_user = User(Name = Name, Username = Username, Password = Password, Email = Email)
 				db.session.add(new_user)
 				db.session.commit()
 				login_user(new_user)
-				session['name'] = Name
-				session['UserName'] = Username
+				session['Name'] = Name
+				session['Username'] = Username
 				return redirect('/user_panel')
 			else:
-				password_match = "Passwords could not match!"
+				password_match = "Passwords are not same!"
 				return render_template('/sign_up.html', password_match = password_match)
 	else:
 	    return render_template('sign_up.html')
@@ -79,40 +84,48 @@ def login():
 		if usr:
 			if usr.Password == Password:
 				login_user(usr)
-				session['name'] = usr.Name
-				session['UserName'] = usr.Username
+				session['Name'] = usr.Name
+				session['Username'] = usr.Username
 				return redirect('/user_panel')
 			else:
 			    password_failure = "Password failure!"
-			    return render_template('/login.html',password_failure = password_failure)
+			    return render_template('/login.html', password_failure = password_failure)
 		else:
-			name_error = "Username not found!"
-			return render_template('/login.html',name_error = name_error)
+			name_failure = "Username not found!"
+			return render_template('/login.html', name_failure = name_failure)
 
 @app.route('/join',methods=['GET','POST'])
 def join():
 	if request.method == 'GET':
 		return render_template('join.html')
 	else:
-		channel_name = request.form['channel_name']
-		nickname = request.form['NickName']
-		session['channel_name'] = channel_name
-		session['nickname'] = nickname
-		chnn = Channel.query.filter_by(channel_name=channel_name).first()
+		Channel_Name = request.form['Channel_Name']
+		Channel_Password = request.form['Channel_Password']
+		Nickname = request.form['Nickname']
+		session['Channel_Name'] = Channel_Name
+		session['Nickname'] = Nickname
+
+		chnn = Channel.query.filter_by(Channel_Name=Channel_Name).first()
 		if not chnn:
-			ChannelName_error = "Sorry, channel name is not found!"
-			return render_template('join.html',ChannelName_error = ChannelName_error)
-		nick = Channel.query.filter_by(nickname=nickname).first()
+			ChannelName_failure = "Sorry, channel name is not found!"
+			return render_template('join.html', ChannelName_failure = ChannelName_failure)
+
+		if chnn.Channel_Password != Channel_Password:
+			ChannelPassword_failure = "Channel name and password could not match!"
+			return render_template('join.html', ChannelPassword_failure = ChannelPassword_failure)
+
+		nick = Channel.query.filter_by(Nickname=Nickname).first()
 		if nick:
-			NickName_failure = "This nickname is already being used!"
-			return render_template('join.html',NickName_failure = NickName_failure)
-		return redirect(url_for('channel'))
+			Nickname_failure = "This nickname is already being used!"
+			return render_template('join.html', Nickname_failure = Nickname_failure)
+
+		return redirect('/channel')
 
 @app.route('/log_out')
 @login_required
 def log_out():
 	logout_user()
-	print(session.get('name') + " has logged out.")
+	print(session.get('Name') + " has logged out.")
 	return 'Logged out.'
 
 @app.route('/user_panel',methods=['GET','POST'])
@@ -121,60 +134,55 @@ def user_panel():
 	if request.method == 'GET':
 		return render_template('user_panel.html')
 	else:
-		name = session['name']
-		user = session['UserName']
-		channel_name = request.form['Channel_Name']
-		nickname = request.form['NickName']
-		chn = Channel.query.filter_by(channel_name=channel_name).first()
+		Name = session['Name']
+		User = session['Username']
+		Channel_Name = request.form['Channel_Name']
+		Nickname = request.form['Nickname']
+		Channel_Password = request.form['Channel_Password']
+		chn = Channel.query.filter_by(Channel_Name=Channel_Name).first()
 		if chn:
-			ChannelName_error = "Channel is already exists please choose another."
-			return render_template('user_panel.html', ChannelName_error = ChannelName_error)
+			ChannelName_failure = "Channel is already exists please choose another."
+			return render_template('user_panel.html', ChannelName_failure = ChannelName_failure)
 
-		nick = Channel.query.filter_by(nickname=nickname).first()
+		nick = Channel.query.filter_by(Nickname=Nickname).first()
 		if nick:
 			NickName_failure = "Nickname is already being used now try something else."
 			return render_template('user_panel.html',NickName_failure = NickName_failure)
-		new_channel = Channel(Username = user, nickname = nickname,channel_name = channel_name)
-		session['nickname'] = nickname
-		session['channel_name'] = channel_name
+		new_channel = Channel(Username = User, Nickname = Nickname, Channel_Name = Channel_Name, Channel_Password = Channel_Password)
+		session['Nickname'] = Nickname
+		session['Channel_Name'] = Channel_Name
 		db.session.add(new_channel)
 		db.session.commit()
-		return redirect(url_for('channel'))
+		return redirect('/channel')
 
 @app.route('/channel',methods=['GET','POST'])
 def channel():
-    print(session.get('nickname') + " created channel " + session.get('channel_name'))
-    nickname = session.get('nickname')
-    channel_name = session.get('channel_name')
-    return render_template('channel.html', nickname=nickname, channel_name=channel_name)
+    print(session.get('Nickname') + " created channel " + session.get('Channel_Name'))
+    Nickname = session.get('Nickname')
+    Channel_Name = session.get('Channel_Name')
+    return render_template('channel.html', Nickname=Nickname, Channel_Name=Channel_Name)
 
 @socketio.on('joined',namespace='/channel')
 def joined(message):
-    """ Sent by clients when they enter a channel.
-    A status message is broadcast to all people in the channel. """
-    print(session.get('nickname') + " entered the channel " + session.get('channel_name'))
-    channel_name = session.get('channel_name')
-    nickname=session.get('nickname')
-    join_room(channel_name)
-    emit('status', {'msg': nickname + ' has entered the channel.'}, room=channel_name)
+    print(session.get('Nickname') + " entered the channel " + session.get('Channel_Name'))
+    Channel_Name = session.get('Channel_Name')
+    Nickname=session.get('Nickname')
+    join_room(Channel_Name)
+    emit('status', {'msg': Nickname + ' has entered the channel.'}, room=Channel_Name)
 
 @socketio.on('text',namespace='/channel')
 def text(message):
-    """ Sent by a client when the user entered a new message.
-    The message is sent to all people in the channel. """
-    print("Sent message by " + session.get('nickname') + ": " + message['msg'])
-    channel_name = session.get('channel_name')
-    nickname=session.get('nickname')
-    emit('message', {'msg': nickname + ': ' + message['msg']}, room=channel_name)
+    print("Sent message by " + session.get('Nickname') + ": " + message['msg'])
+    Channel_Name = session.get('Channel_Name')
+    Nickname=session.get('Nickname')
+    emit('message', {'msg': Nickname + ': ' + message['msg']}, room=Channel_Name)
 
 @socketio.on('left',namespace='/channel')
 def left(message):
-    """ Sent by clients when they leave a channel.
-    A status message is broadcast to all people in the channel. """
-    print(session.get('nickname') + " leaved the channel " + session.get('channel_name'))
-    channel_name = session.get('channel_name')
-    leave_room(channel_name)
-    emit('status', {'msg': session.get('nickname') + ' has left the channel.'}, room=channel_name)
+    print(session.get('Nickname') + " leaved the channel " + session.get('Channel_Name'))
+    Channel_Name = session.get('Channel_Name')
+    leave_room(Channel_Name)
+    emit('status', {'msg': session.get('Nickname') + ' has left the channel.'}, room=Channel_Name)
 
 if __name__ == '__main__':
 	socketio.run(app)
