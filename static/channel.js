c8506d23be806a57718949b9384c9acc5a0890da
n1rv4n4 <32897657+n1rv4n4@users.noreply.github.com>
2017-11-26 14:59:42 +0300

    Clean code and design improvements.

---
+++ static/channel.js
@@ -0,0 +1,29 @@
+      var socket;
+      $(document).ready(function(){
+          socket = io.connect('http://' + document.domain + ':' + location.port + '/channel');
+          socket.on('connect', function() {
+              socket.emit('joined', {});
+          });
+          socket.on('status', function(data) {
+              $('#chat').val($('#chat').val() + '<' + data.msg + '>\n');
+              $('#chat').scrollTop($('#chat')[0].scrollHeight);
+          });
+          socket.on('message', function(data) {
+              $('#chat').val($('#chat').val() + data.msg + '\n');
+              $('#chat').scrollTop($('#chat')[0].scrollHeight);
+          });
+          $('#text').keypress(function(e) {
+              var code = e.keyCode || e.which;
+              if (code == 13) {
+                  text = $('#text').val();
+                  $('#text').val('');
+                  socket.emit('text', {msg: text});
+              }
+          });
+      });
+      function leave_room() {
+          socket.emit('left', {}, function() {
+              socket.disconnect();
+              window.location.href = "{{ url_for('user_panel') }}";
+          });
+      }
